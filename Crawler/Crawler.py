5e067816aad26801419ecf5c853babe6593cee05
gtaumaturgo <gtaumaturg@gmail.com>
2018-05-23 18:07:44 -0300

    now the crawler is able to fetch and download all the DOU from a single day

--- Crawler/Crawler.py
+++ Crawler/Crawler.py
@@ -1,14 +1,22 @@
 import spade
-import urllib
+import urllib2
+url_prefix = "http://pesquisa.in.gov.br/imprensa/servlet/INPDFViewer?jornal=515&pagina="
+url_suffix = "&data=23/05/2018&captchafield=firstAccess"
 
 class Crawler(spade.Agent.Agent):
 	class CheckBehav(spade.Behaviour.OneShotBehaviour):
 		def onStart(self):
 			pass
-        def _process(self):
-            pass
-        def onEnd(self):
-            pass
+		def _process(self):
+			for i in range(1,75):
+				url = url_prefix + str(i) + url_suffix
+				response = urllib2.urlopen(url)
+				file = open("documents/document" + str(i) + ".pdf",'wb')
+				file.write(response.read())
+				file.close()
+		def onEnd(self):
+			print("Hehe!")
+			exit(0)
 
 	def _setup(self):
 		b = self.CheckBehav()
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Projeto-DOU
+
+Projeto em python 2.7
+## Instalação
+
+instale o spade
+
+```
+pip2 install spade --user
+```
